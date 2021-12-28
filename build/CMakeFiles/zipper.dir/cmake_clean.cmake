3885a224d78e8c80bb069810c24af5a752768252
SaintComorek <comorek7@zoznam.sk>
2021-12-28 23:57:53 +0100

    Alpha verzia

---
+++ build/CMakeFiles/zipper.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zipper.dir/main.c.o"
+  "zipper"
+  "zipper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/zipper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
